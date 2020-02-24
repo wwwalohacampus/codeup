a6c40c7ef2d37fec0dc9e54723b63e57d6fe7581
ALOHA <ALOHA@DESKTOP-FBBK53F>
2020-02-24 20:59:00 +0900

    sort

--- src/codeup/sort/_1442.java
+++ src/codeup/sort/_1442.java
@@ -1,5 +1,31 @@
 package codeup.sort;
 
-public class _1442 {
+import java.util.Scanner;
 
+public class _1442 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		// [1][3][2][5][4] 
+		for (int i = 0; i < n; i++) {
+			for (int j = i+1; j < n ; j++) {
+				if( arr[i] > arr[j] ) {
+					// 교환패턴 (swap)
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < n ; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }
