4f78a44af366c25708ec6b490d6f36e073447f92
aloha <floaholic@nate.com>
2020-04-26 00:37:14 +0900

    1000s

---
+++ src/codeup/_1000s/_1003.java
@@ -0,0 +1,7 @@
+package codeup._1000s;
+
+public class _1003 {
+	public static void main(String[] args) {
+		System.out.println("Hello\nWorld");
+	}
+}
---
+++ src/codeup/_1000s/_1007.java
@@ -0,0 +1,7 @@
+package codeup._1000s;
+
+public class _1007 {
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+}
